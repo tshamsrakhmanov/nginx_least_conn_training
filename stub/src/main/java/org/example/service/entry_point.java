aeec3266bd343e29379106b3750fef3f3629e676
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2025-02-12 09:28:16 +0400

    minor

---
+++ stub/src/main/java/org/example/service/entry_point.java
@@ -0,0 +1,13 @@
+package org.example.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class entry_point {
+
+    public static void main(String[] args) {
+        SpringApplication.run(entry_point.class, args);
+    }
+
+}
