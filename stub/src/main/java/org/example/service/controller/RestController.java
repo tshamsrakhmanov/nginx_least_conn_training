079c45aa826fe6b901de7c604f4783cb4bf4c0a3
Timur Shmarakhmanov <t.shamsrakhmanov@gmail.com>
2025-02-12 18:57:18 +0400

    temp temp

--- stub/src/main/java/org/example/service/controller/RestController.java
+++ stub/src/main/java/org/example/service/controller/RestController.java
@@ -2,6 +2,7 @@ package org.example.service.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -12,6 +13,7 @@ public class RestController {
 
     @GetMapping(value = "/hello")
     public String hello() {
+
         log.info(" *** RESULT *** Hello");
         return "SUCCESS!";
     }
--- stub/src/main/java/org/example/service/entry_point.java
+++ stub/src/main/java/org/example/service/entry_point.java
@@ -3,12 +3,12 @@ package org.example.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.Duration;
+
 @SpringBootApplication
 public class entry_point {
 
     public static void main(String[] args) {
-
         SpringApplication.run(entry_point.class, args);
     }
-
 }
