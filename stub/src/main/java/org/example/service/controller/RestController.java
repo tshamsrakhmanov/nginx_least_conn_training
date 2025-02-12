e9ae581f331e8d868a114333998daccc09e2a127
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2025-02-12 09:27:03 +0400

    init commit. todo's: metrics, dockerfile, docker-compose, influx\grafana, nginx conf

---
+++ stub/src/main/java/org/example/service/controller/RestController.java
@@ -0,0 +1,18 @@
+package org.example.service.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    private final Logger log = LoggerFactory.getLogger(RestController.class);
+
+    @GetMapping(value = "/hello")
+    public String hello() {
+        log.info(" *** RESULT *** ");
+        return "SUCCESS!";
+    }
+}
