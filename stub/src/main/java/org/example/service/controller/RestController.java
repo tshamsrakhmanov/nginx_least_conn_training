1ebd48e3df74ec278d752b4ec1bd3c52e8b076da
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2025-02-18 12:23:30 +0300

    custom delay add -> configured by arg passed to jar

--- stub/src/main/java/org/example/service/controller/RestController.java
+++ stub/src/main/java/org/example/service/controller/RestController.java
@@ -3,7 +3,9 @@ package org.example.service.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @org.springframework.web.bind.annotation.RestController
@@ -11,10 +13,13 @@ public class RestController {
 
     private final Logger log = LoggerFactory.getLogger(RestController.class);
 
-    @GetMapping(value = "/hello")
-    public String hello() {
+    @Value("${custom.response}")
+    private Integer custom_response;
 
-        log.info(" *** RESULT *** Hello");
+    @GetMapping(value = "/hello")
+    public String hello() throws InterruptedException {
+        Thread.sleep(custom_response);
+        log.info(" *** RESULT *** Hello  *** " + custom_response.toString());
         return "SUCCESS!";
     }
 }
--- stub/src/main/java/org/example/service/entry_point.java
+++ stub/src/main/java/org/example/service/entry_point.java
@@ -2,13 +2,23 @@ package org.example.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.CommandLineRunner;
+
 
 import java.time.Duration;
 
 @SpringBootApplication
 public class entry_point {
 
+
+
     public static void main(String[] args) {
+
+        for(String arg:args) {
+            System.out.println(arg);
+        }
+
         SpringApplication.run(entry_point.class, args);
     }
 }
