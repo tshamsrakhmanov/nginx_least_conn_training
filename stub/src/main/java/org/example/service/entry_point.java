b3a47b9c6225e55f1421868b5be82c2e60ccb2e7
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2025-02-19 08:22:49 +0400

    add dashboard config for grafana

--- stub/src/main/java/org/example/service/entry_point.java
+++ stub/src/main/java/org/example/service/entry_point.java
@@ -15,13 +15,15 @@ public class entry_point {
 
     public static void main(String[] args) {
 
-        System.out.println("JAVA ARGS:::");
+        System.out.println("JAVA ARGS::: -->");
+        System.out.println();
 
         for(String arg:args) {
             System.out.println(arg);
         }
 
-        System.out.println(":::JAVA ARGS");
+        System.out.println();
+        System.out.println("<-- :::JAVA ARGS");
 
 
         SpringApplication.run(entry_point.class, args);
